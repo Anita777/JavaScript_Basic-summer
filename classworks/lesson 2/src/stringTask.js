9ebad10c671ad8fee2ac021706e3ced2e5dca851
Oleg <oleglustenko@gmail.com>
2017-05-16 18:41:18 +0300

    lesson 2

---
+++ classworks/lesson 2/src/stringTask.js
@@ -0,0 +1,41 @@
+let iString = 'privet ya stroka';
+let string = "STROKA";
+let ecma2015String = `Hi, I'am String`;
+let privetkakdela = 'hello';
+let hello = '';
+let world = 'world';
+
+
+//console.log(helloWorld) // hello world
+//console.log(privetkakdela, privetkakdela.length) // hello world
+
+// Создайте объект
+// Добавьте данному объекту свойства myCars, speed
+// выведите в консоле предложение ->
+    
+// Все мои bmw, vaz ездят на скорости 360
+
+
+
+const myNameIs = 'oLEG';
+const correctName = // Oleg
+
+
+// напишите конструкцию, которая будет превращать ваше имя написанное в таком же регистре как у меня
+// в имя с заглавной буквы и остальными буквами в маленьком регистре
+
+
+
+var test = 'JaScript';
+//console.log(test) // JavaScript
+
+
+// google -> mdn
+
+/*
+ Для того, чтобы сделать переменную которая будет являться строкой, необходимо
+ использовать одну из интерполяции строки
+ Одинарные кавычки ' '
+ Двойные кавычки " "
+ Ё или консоль ` `
+ */
