58a93b545b59b636b6bdb5be2196ffcae87c15e7
Oleg <oleglustenko@gmail.com>
2017-05-23 21:28:10 +0300

    lesson 4

--- classworks/lesson 3/src/homework.js
+++ classworks/lesson 3/src/homework.js
@@ -3,27 +3,59 @@
 с свойством name, а значением будет первый аргумент функции
 */
 
+function returnObject(qweqwevqwe1v231v23) {
+  let obj = { name: qweqwevqwe1v231v23 };
+  // if (true) {
+  //   obj.family = 'my family is cool'
+  // } else {
+  //   obj.family = 'my family is cool'
+  // }
+  return obj;
+  return { name: qweqwevqwe1v231v23 };
+}
+
+var someObject = returnObject('Oleg');
+console.log(someObject);
+
 /*
+
 Функция будет принимать 1 аргумент - ОБЪЕКТ у которого
 будет свойство name
 и возвращать новый объект изменяя свойство name
 в большой регистр
-*/
-// На дом ! кто не сделал
 
+*/
+
+function toUpperCase(obj) {
+  return { name: obj.name.toUpperCase() };
+}
+const objectName = { name: 'privet kak dela' };
+console.log(toUpperCase(objectName));
 
 /*
 1. Напишите функцию которая принимает в 
-качестве аргумента массив
-и элемент массива и добавляет элемент в конец массива
+качестве аргумента массив и элемент массива 
+и добавляет элемент в конец массива
 */
 
 let arr = [];
+arr[0] = 'privet';
+arr[1] = 'poka';
+arr[2] = 'zdrastvuite';
 
 function addToArray(arrParam, newElem) {
- // без естественных способов добавления
+  console.log(arrParam, arrParam.length);
+
+  // arr[arrParam.length] = newElem;
+
+  arr.push(newElem);
+
+  // без естественных способов добавления
 }
 
+// addToArray(arr, 34);
+// addToArray(arr, 55);
+// console.log(arr);
 
 /*
 2. Напишите функцию которая получает 3 параметра 
@@ -32,26 +64,32 @@ function addToArray(arrParam, newElem) {
       argument1: param1,
       argument2: param2,
       argument3: param3,
-    }    
+    }   
 */
 
-console.log(simpleObjectGenerator('protocol', { url: '22' }, 8000));
+function simpleObjectGenerator(one, two, three) {
+  return {
+    argument1: one,
+    argument2: two,
+    argument3: three
+  };
+}
 
+console.log(simpleObjectGenerator('protocol', { url: '22' }, 8000));
 /* {
    argument1:'protocol',
    argument2:{url:'22'},
    argument3:8000
 }*/
 
-var userNames = [{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }];
+// var userNames = [{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }];
 
-console.log(simpleObjectGenerator(77, userNames, 'privet kak dela chto novogo'.toUpperCase()));
+// console.log(simpleObjectGenerator(77, userNames, 'privet kak dela chto novogo'.toUpperCase()));
 /* {
  argument1:'77',
  argument2:[{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }],
  argument3:PRIVET KAK DELA CHTO NOVOGO
  }*/
-
 
 ///////////////////////////////////////////////
 /*
@@ -63,14 +101,24 @@ console.log(simpleObjectGenerator(77, userNames, 'privet kak dela chto novogo'.t
     Проверяет если есть свойство name в переданном объекте, 
     тогда добавляет данное свойство и возвращает новый объект
 
+*/
 var myName = { name: 'Oleg' };
 
 function addNameToUser(newKey, newValue, userName) {
+  const obj = {};
+  obj[newKey] = newValue;
 
+  obj.name = obj.name || userName.name;
+  // if (userName.name !== undefined) {
+  //   obj.name = userName.name;
+  // }
+  // obj.newKey = newValue;
+
+  return obj;
 }
-*/
 
 console.log(addNameToUser('family', '%Lustenko%', myName));
+console.log(addNameToUser('name', 'privet Pasha', {}));
 /* {name:'Oleg', family:'%Lustenko%'} */
 
 console.log(myName);
@@ -100,8 +148,22 @@ fizzBuzz(15); // 'FizzBuzz'
 fizzBuzz(21); // 'Fizz'
 
 /* -> @@ SUPER 2
+
 Напишите функцию, которая будет принимать 4 аргумента, 
 последний всегда функция.
+
 Добавьте первые 3 аргумента в массив и вызовите переданную функцию с данным массивом
 
 */
+
+function super2(arg1, arg2, arg3, callback) {
+  var myArr = [arg1, arg2, arg3];
+  console.log(arg1);
+  console.log(arg2);
+  console.log(arg3);
+  callback(myArr);
+}
+var qwerty = function(newArray) {
+  console.log(newArray);
+};
+super2(10, 20, 30, qwerty);
---
+++ classworks/lesson 4/_homework/main.js
@@ -0,0 +1,49 @@
+/*
+ 1. Напишите функцию которая принимает 2 числа 
+ и возвращает массив содержащий числа между первым числом и вторым числом;
+ */
+
+function numbersBetween(a, b) {}
+
+console.log(numbersBetween(1, 5)); // 2,3,4
+console.log(numbersBetween(3, 6)); // 4,5
+console.log(numbersBetween(12, 15)); // 13,14
+console.log(numbersBetween(10, 15)); // 11, 12, 13, 14
+
+/*
+ 2. Перепешите задачу FizzBuzz, но с использованием цикла. 
+ Расчет чисел должен идти до 100 
+ */
+
+/*
+ 3. Напишите функцию которая принимает 1 аргумент - массив
+ И возвращает новый массив содержащий типы значений переменных
+ */
+
+let arr = [1, null, undefined, 'str', {}, [], function() {}];
+
+/*
+ @@SUPER@@. Вам дан массив array, содержащий внутри объекты. 
+ Напишите функцию которая внутри цикла проходится по каждому элементу массива
+ И проверяет какой тип данных содержит свойство age, если тип данных NaN, 
+ тогда добавляет данному объекту свойство unknownAge: true
+
+ На основании нового массива, создайте новую функцию, которая вернет новый массив 
+  содержащий все объекты содержащие свойство unknownAge:true
+ */
+
+let array = Array.from({ length: 35 }).map(
+  (value, index) => (index % 2 ? { age: index + 2 } : { age: NaN })
+);
+
+console.log(array); // [ {age:NaN}, {age:3}, {age:NaN}, {age:5}, {age:NaN}, {age:7} ....]
+console.log(array.length); // 35
+
+function solution(arr) {}
+
+let arr = ['privet', 12, {}, [1, 2]];
+let arr2 = ['privet', 12, {}, [1, 2]];
+
+function returnArr(argument) {}
+returnArr(arr);
+returnArr(arr2);
