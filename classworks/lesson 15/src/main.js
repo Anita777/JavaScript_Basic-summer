aae5a483264833b1f1340c3cf60197d54e114f48
Oleg <oleglustenko@gmail.com>
2017-06-30 19:51:14 +0300

    lesson 15 upd

--- classworks/lesson 15/src/main.js
+++ classworks/lesson 15/src/main.js
@@ -1,4 +1,5 @@
 const app = {
+  appName: 'Тест по программированию',
   questions: [
     {
       name: 'Вопрос 1',
@@ -16,31 +17,47 @@ const app = {
       rightAnswers: [2]
     } // приходят с сервера
   ],
+
   createLi() {
-    return this.new('li')
+    return this.new('li');
+  },
+  createAnswer(answer) {
+    const liAnswer = this.createLi();
+    const label = this.new('label');
+    label.textContent = answer;
+    const input = this.new('input');
+    input.setAttribute('type', 'checkbox');
+    liAnswer.appendChild(input);
+    console.log(input.getAttribute('type'));
+    liAnswer.appendChild(label);
+    return liAnswer;
   },
   render() {
-    const body = document.body;   
+    const body = document.body;
     const main = this.new('main');
     const h1 = this.new('h1');
+    h1.textContent = this.appName;
     const ol = this.new('ol');
 
     main.appendChild(h1);
     main.appendChild(ol);
-    
+
     this.questions.forEach(question => {
       const li = this.createLi();
       const h3 = this.new('h3');
+      h3.textContent = question.name;
       const ul = this.new('ul');
       question.answers.forEach(answer => {
-        const liAnswer = this.createLi();
-        
-
-      })
+        /* ---------------------------- */
+        const htmlAnswer = this.createAnswer(answer);
+        /* ---------------------------- */
+        ul.appendChild(htmlAnswer);
+      });
       li.appendChild(h3);
       li.appendChild(ul);
-
-    })    
+      ol.appendChild(li);
+    });
+    body.appendChild(main);
   },
   new(tag) {
     return document.createElement(tag);
